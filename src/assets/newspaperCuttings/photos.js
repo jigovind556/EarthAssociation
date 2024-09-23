136f90adae54802e7eabcd2bb35418f810eb1150
jigovind556 <jigovind556@gmail.com>
2024-09-23 17:15:04 +0530

    Completed Gallery section

---
+++ src/assets/newspaperCuttings/photos.js
@@ -0,0 +1,201 @@
+import p1 from './newspaper_cuttings_page-0001.jpg';
+import p2 from './newspaper_cuttings_page-0002.jpg';
+import p3 from './newspaper_cuttings_page-0003.jpg';
+import p4 from './newspaper_cuttings_page-0004.jpg';
+import p5 from './newspaper_cuttings_page-0005.jpg';
+import p6 from './newspaper_cuttings_page-0006.jpg';
+import p7 from './newspaper_cuttings_page-0007.jpg';
+import p8 from './newspaper_cuttings_page-0008.jpg';
+import p9 from './newspaper_cuttings_page-0009.jpg';
+import p10 from './newspaper_cuttings_page-0010.jpg';
+import p11 from './newspaper_cuttings_page-0011.jpg';
+import p12 from './newspaper_cuttings_page-0012.jpg';
+import p13 from './newspaper_cuttings_page-0013.jpg';
+import p14 from './newspaper_cuttings_page-0014.jpg';
+import p15 from './newspaper_cuttings_page-0015.jpg';
+import p16 from './newspaper_cuttings_page-0016.jpg';
+import p17 from './newspaper_cuttings_page-0017.jpg';
+import p18 from './newspaper_cuttings_page-0018.jpg';
+import p19 from './newspaper_cuttings_page-0019.jpg';
+import p20 from './newspaper_cuttings_page-0020.jpg';
+import p21 from './newspaper_cuttings_page-0021.jpg';
+import p22 from './newspaper_cuttings_page-0022.jpg';
+import p23 from './newspaper_cuttings_page-0023.jpg';
+import p24 from './newspaper_cuttings_page-0024.jpg';
+import p25 from './newspaper_cuttings_page-0025.jpg';
+import p26 from './newspaper_cuttings_page-0026.jpg';
+import p27 from './newspaper_cuttings_page-0027.jpg';
+import p28 from './newspaper_cuttings_page-0028.jpg';
+import p29 from './newspaper_cuttings_page-0029.jpg';
+import p30 from './newspaper_cuttings_page-0030.jpg';
+import p31 from './newspaper_cuttings_page-0031.jpg';
+import p32 from './newspaper_cuttings_page-0032.jpg';
+import p33 from './newspaper_cuttings_page-0033.jpg';
+
+export const photos = [
+  {
+    src: p1,
+    width: 1240,
+    height: 1755
+  },
+  {
+    src: p2,
+    width: 1240,
+    height: 1755
+  },
+  {
+    src: p3,
+    width: 1240,
+    height: 1755
+  },
+  {
+    src: p4,
+    width: 1240,
+    height: 1755
+  },
+  {
+    src: p5,
+    width: 1240,
+    height: 1755
+  },
+  {
+    src: p6,
+    width: 1240,
+    height: 1755
+  },
+  {
+    src: p7,
+    width: 1240,
+    height: 1755
+  },
+  {
+    src: p8,
+    width: 1755,
+    height: 1240
+  },
+  {
+    src: p9,
+    width: 1755,
+    height: 1240
+  },
+  {
+    src: p10,
+    width: 1755,
+    height: 1240
+  },
+  {
+    src: p11,
+    width: 1240,
+    height: 1755
+  },
+  {
+    src: p12,
+    width: 1240,
+    height: 1755
+  },
+  {
+    src: p13,
+    width: 1240,
+    height: 1755
+  },
+  {
+    src: p14,
+    width: 1240,
+    height: 1755
+  },
+  {
+    src: p15,
+    width: 1240,
+    height: 1755
+  },
+  {
+    src: p16,
+    width: 1240,
+    height: 1755
+  },
+  {
+    src: p17,
+    width: 1240,
+    height: 1755
+  },
+  {
+    src: p18,
+    width: 1240,
+    height: 1755
+  },
+  {
+    src: p19,
+    width: 1240,
+    height: 1755
+  },
+  {
+    src: p20,
+    width: 1755,
+    height: 1240
+  },
+  {
+    src: p21,
+    width: 1240,
+    height: 1755
+  },
+  {
+    src: p22,
+    width: 1755,
+    height: 1240
+  },
+  {
+    src: p23,
+    width: 1240,
+    height: 1755
+  },
+  {
+    src: p24,
+    width: 1240,
+    height: 1755
+  },
+  {
+    src: p25,
+    width: 1240,
+    height: 1755
+  },
+  {
+    src: p26,
+    width: 1755,
+    height: 1240
+  },
+  {
+    src: p27,
+    width: 1240,
+    height: 1755
+  },
+  {
+    src: p28,
+    width: 1240,
+    height: 1755
+  },
+  {
+    src: p29,
+    width: 1240,
+    height: 1755
+  },
+  {
+    src: p30,
+    width: 1755,
+    height: 1240
+  },
+  {
+    src: p31,
+    width: 1755,
+    height: 1240
+  },
+  {
+    src: p32,
+    width: 1240,
+    height: 1755
+  },
+  {
+    src: p33,
+    width: 1240,
+    height: 1755
+  },
+]
--- src/pages/gallery/Gallery.jsx
+++ src/pages/gallery/Gallery.jsx
@@ -1,23 +1,78 @@
-import React from 'react';
+import React, { useState } from "react";
+import RGallery from "react-photo-gallery";
+import { photos } from "../../assets/wildlifeWeek/photos";
+import { photos as photos2 } from "../../assets/newspaperCuttings/photos";
 
 const Gallery = () => {
+  // State to control "see more" functionality
+  const [seeMoreWildlife, setSeeMoreWildlife] = useState(false);
+  const [seeMoreNewspaper, setSeeMoreNewspaper] = useState(false);
+
   return (
     <main className="my-[65px]">
-        <section className="mx-auto max-w-[1140px]">
-            <h1 className="text-center text-[#333] font-raleway font-bold text-[36px] lg:text-[48px]">
-            Gallery
-            </h1>
-            <div className="grid grid-cols-1 gap-4 mt-8 lg:grid-cols-3">
-            <div className="bg-[#FFF000] h-[300px]"></div>
-            <div className="bg-[#FFF000] h-[300px]"></div>
-            <div className="bg-[#FFF000] h-[300px]"></div>
-            <div className="bg-[#FFF000] h-[300px]"></div>
-            <div className="bg-[#FFF000] h-[300px]"></div>
-            <div className="bg-[#FFF000] h-[300px]"></div>
-            </div>
-        </section>
+      <section className="mx-auto max-w-[1140px]">
+        {/* Wildlife Week Section */}
+        <h1 className="text-center text-[#333] font-raleway font-bold text-[36px] lg:text-[48px]">
+          Gallery
+        </h1>
+        <div className="relative">
+          {/* Gallery with gradient overlay */}
+          <div
+            className={`relative overflow-hidden transition-all duration-500 ${
+              seeMoreWildlife ? "max-h-[100%]" : "max-h-[400px]"
+            }`}
+          >
+            <RGallery photos={photos} />
+          </div>
+
+          {/* Gradient Overlay */}
+          {!seeMoreWildlife && (
+            <div className="absolute inset-0 bg-gradient-to-b from-transparent to-black opacity-60"></div>
+          )}
+
+          {/* See More Button */}
+          <div className="absolute bottom-4 left-1/2 transform -translate-x-1/2">
+            <button
+              onClick={() => setSeeMoreWildlife(!seeMoreWildlife)}
+              className="px-6 py-2 bg-green-500 text-white font-semibold rounded-full hover:bg-green-600 transition duration-300"
+            >
+              {seeMoreWildlife ? "See Less" : "See More"}
+            </button>
+          </div>
+        </div>
+
+        {/* Newspaper Cuttings Section */}
+        <h1 className="text-center text-[#333] font-raleway font-bold text-[36px] lg:text-[48px] mt-12">
+          Newspaper Cuttings
+        </h1>
+        <div className="relative">
+          {/* Gallery with gradient overlay */}
+          <div
+            className={`relative overflow-hidden transition-all duration-500 ${
+              seeMoreNewspaper ? "max-h-[100%]" : "max-h-[400px]"
+            }`}
+          >
+            <RGallery photos={photos2} />
+          </div>
+
+          {/* Gradient Overlay */}
+          {!seeMoreNewspaper && (
+            <div className="absolute inset-0 bg-gradient-to-b from-transparent to-black opacity-60"></div>
+          )}
+
+          {/* See More Button */}
+          <div className="absolute bottom-4 left-1/2 transform -translate-x-1/2">
+            <button
+              onClick={() => setSeeMoreNewspaper(!seeMoreNewspaper)}
+              className="px-6 py-2 bg-green-500 text-white font-semibold rounded-full hover:bg-green-600 transition duration-300"
+            >
+              {seeMoreNewspaper ? "See Less" : "See More"}
+            </button>
+          </div>
+        </div>
+      </section>
     </main>
   );
-}
+};
 
 export default Gallery;
